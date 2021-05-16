16696e970915cb3ad751439c8032aaf5ad099527
Joe Weller <josephweller@protonmail.com>
2021-05-16 22:24:16 +0100

    changes

--- helpers.js
+++ helpers.js
@@ -1,14 +1,51 @@
 const https = require('https');
+const zlib = require('zlib');
+const fs = require('fs');
 
-exports = {
-    getRequest
-};
 
-const makeDiscordOptions = function(url) {
+const makeApiDate = function() {
 
+    const normaliseString = function(n) {
+        let result = (n).toString()
+        if (1 === result.length) {
+            result = `0${result}`
+        }
+        return result
+    }
+
+    const d = new Date(Date.now());
+    
+    return `${d.getFullYear()}-${normaliseString(d.getMonth() + 1)}-${normaliseString(d.getDate())}`
 }
 
-const getRequest = async function(options, payload) {
+const waitMinutes = async function(m) {
+    return waitSeconds(m * 60)
+}
+
+const waitSeconds = async function(s) {
+    return waitMilliSeconds(s * 1000);
+}
+
+const waitMilliSeconds = async function(ms) {
+    return new Promise(
+        resolve => setTimeout(resolve, ms)
+    )
+}
+
+const gzipDecompress = async function(buffer) {
+    return new Promise((resolve, reject) => {
+        zlib.unzip(buffer, async (err, buffer) => {
+            
+            if (!err) {
+                resolve(buffer.toString())
+            } else {
+                reject(err)
+            }
+        });
+    })
+}
+
+const fetchRequest = async function(options, payload) {
     return new Promise((resolve, reject) => {
         const req = https.request(
             options, (res) => {
@@ -22,13 +59,20 @@ const getRequest = async function(options, payload) {
                     reject(`Request callout error: ${err}`);
                 });
                 
-                res.on('end', () => {
-                    console.log(`statuscode: ${res.statusCode}`);
-                    const responseBody = Buffer.concat(chunks).toString();
-                    if (res.statusCode > 200 && res.statusCode >= 300) {
-                        reject(`Callout statuscode: ${res.statuscode}. body: ${responseBody}`);
+                res.on('end', async () => {
+
+                    var body;
+
+                    if (res.headers["content-encoding"] && res.headers["content-encoding"] === "gzip") {
+                        body = await gzipDecompress(Buffer.concat(chunks))
                     } else {
-                        resolve({"response": res, "body": responseBody});
+                        body = Buffer.concat(chunks).toString()
+                    }
+
+                    if (res.statusCode > 200 && res.statusCode >= 300) {
+                        reject(`Callout statuscode: ${res.statuscode}. body: ${body}`);
+                    } else {
+                        resolve({"response": res, "body": body});
                     };
                 });
             }
@@ -38,9 +82,62 @@ const getRequest = async function(options, payload) {
             reject(`Request callout error: ${err}`);
         });
 
-        if (payload) { req.write(payload) }
-        else { req.write() }
+        if ("post" === options.method) {
 
+            if (payload) { req.write(payload) }
+            else { req.write() }
+        }
+        
         req.end();
     });
 }
+
+
+const sendDiscordWebhook = async function(options, message) {
+    return new Promise((resolve, reject) => {
+
+        const req = https.request(
+            options, (res) => {
+                
+                const chunks = [ ];
+
+                res.on('data', (chunk) => {
+                    chunks.push(chunk);
+                });
+                
+                res.on('error', (err) => {
+                    reject(`Discord response callout error: ${err}`);
+                });
+                
+                res.on('end', () => {
+                    if (res.statusCode > 200 && res.statusCode >= 300) {
+                        reject(`Discord callout statuscode: ${res.statuscode}. body: ${Buffer.concat(chunks).toString()}`);
+                    } else {
+                        resolve({"response": res});
+                    };
+                });
+            }
+        );
+
+        req.on('error', (err) => {
+            reject(`Discord request callout error: ${err}`);
+        });
+
+        req.write(JSON.stringify({content: message.substring(0, 2000)}));
+        req.end();
+    });
+};
+
+const consoleLog = function(message) {
+    const entry = `${new Date()} - ${message}`
+    console.log(entry)
+    fs.appendFileSync('./app.log', entry + '\n')
+}
+
+module.exports = {
+    consoleLog,
+    fetchRequest,
+    waitMinutes,
+    sendDiscordWebhook,
+    makeApiDate
+};
---
+++ index.js
@@ -0,0 +1,73 @@
+const helpers = require('./helpers');
+const { readFileSync } = require('fs');
+const url = require('url');
+var zlib = require('zlib');
+
+
+var CONFIG;
+
+try {
+    CONFIG = JSON.parse(readFileSync('./config.json'))
+} catch (ex) {
+    console.log(`config file failed with : ${ex.message}`)
+    process.exit(1)
+}
+
+(async function() {
+
+    helpers.consoleLog('started!')
+
+    var covidResult;
+
+    const covidOptions = {
+        hostname: 'api.coronavirus.data.gov.uk',
+        path: `/v1/data?structure=%7B%22date%22%3A%22date%22,%22areaName%22%3A%22areaName%22,%22areaCode%22%3A%22areaCode%22,%22newCasesByPublishDate%22%3A%22newCasesByPublishDate%22,%22cumCasesByPublishDate%22%3A%22cumCasesByPublishDate%22,%22cumDeaths28DaysByPublishDate%22%3A%22cumDeaths28DaysByPublishDate%22,%22cumDeathsByDeathDate%22%3A%22cumDeathsByDeathDate%22%7D&filters=areaType%3Doverview%3Bdate%3D${helpers.makeApiDate()}`,
+        port: 443,
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    }
+
+    // try for 2 hours wiht 10 minute intervals (12 times)
+    for (let i = 0; i < 12; i++) {
+        let result = await helpers.fetchRequest(covidOptions, null)
+        if (200 === result.response.statusCode) {
+            covidResult = result.body
+            helpers.consoleLog(`COVID Response: ${result.response.statusCode}, OK`)
+            break
+        } else {
+            helpers.consoleLog(`COVID Response: ${result.response.statusCode}, waiting 10 minutes`)
+            await helpers.waitMinutes(10);
+        }
+    }
+
+    if (!covidResult) {
+        process.exit(1);
+    } else {
+        covidResult = JSON.parse(covidResult)
+    }
+
+    const discordUrl = new url.URL(CONFIG.discordUrl)
+    const discordOptions = {
+        hostname: discordUrl.hostname,
+        path: discordUrl.pathname,
+        port: 443,
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const discordMessage = `New cases: ${covidResult.data[0].newCasesByPublishDate}`
+    helpers.consoleLog(`discord message: "${discordMessage}"`)
+
+    const discordResult = await helpers.sendDiscordWebhook(discordOptions, discordMessage)
+    if (204 === discordResult.response.statusCode) {
+        helpers.consoleLog(`discord response: ${discordResult.response.statusCode}, OK`)
+    } else {
+        helpers.consoleLog(`discord response: ${discordResult.response.statusCode}, something might be wrong`)
+    }
+
+    helpers.consoleLog('ended!')
+
+})()
